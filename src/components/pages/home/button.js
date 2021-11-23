7b0f9d4011d4971c31dad1a2dfa37a63bd0c0fb2
Maria Cowdrey <mariacowdrey@yahoo.com>
2021-11-22 20:32:17 -0800

    update styles

--- src/components/pages/home/button.js
+++ src/components/pages/home/button.js
@@ -2,30 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 
-
-const STYLES = ['btn--primary', 'btn--outline'];
-
-const SIZES = ['btn--medium', 'btn--large'];
-
 export const Button = ({
   children,
   type,
   onClick,
-  buttonStyle,
-  buttonSize,
-}) => {
-  const checkButtonStyle = STYLES.includes(buttonStyle)
-    ? buttonStyle
-    : STYLES[0];
 
-    const checkButtonSize = SIZES.includes(buttonSize) 
-    ? buttonSize 
-    : SIZES[0]
+}) => {
 
     return (
         <Link to='/sign-up' className='btn-mobile'>
             <button
-            className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+            className='btn'
             onClick={onClick}
             type={type}
             >
--- src/components/pages/home/cards.js
+++ src/components/pages/home/cards.js
@@ -1,7 +1,7 @@
 import React from "react";
 import CardItem from "./carditem";
-import julia from "../../../assets/img/julia-rekamie.jpg";
-import philip from "../../../assets/img/philip-myrtorp.jpg"
+import julia from "../../../assets/img/julia-ross.jpg";
+import andrew from "../../../assets/img/andrew-thompson.jpg"
 
 function Cards() {
   return (
@@ -12,15 +12,15 @@ function Cards() {
           <ul className="cards__items">
             <CardItem 
             src={julia}
-            text='“I think people forget that a happy person is one that has shed many tears. That a person that tries to lift others up, has known the struggle of being pushed down. That a person that looks like they have everything under control, has once tried to gather the broken pieces together. The pain we go through forms our character and allows us to grow. Stop fearing it and realize that your pain is your power.” - Julia Rekamie, Fitness Influencer'
-            label="Julia Rekamie"
+            text='“I think people forget that a happy person is one that has shed many tears. That a person that tries to lift others up, has known the struggle of being pushed down. That a person that looks like they have everything under control, has once tried to gather the broken pieces together. The pain we go through forms our character and allows us to grow. Stop fearing it and realize that your pain is your power.”'
+            label="Julia Ross, Fitness Influencer"
             path="/"
             />
 
             <CardItem 
-            src={philip}
-            text=''
-            label="Philip Myrtorp"
+            src={andrew}
+            text={'"Love of sport, and enthusiasm are much more powerful and healthy tools than anger and negative self-talk, so focusing on the goodness is the most potent source of energy. Reminding myself why I\'m doing what I\'m doing, \'my dreams,\' helps me to focus on the long-term reward versus the short-term pain or distraction."'}
+            label="Andrew Thompson, Celebrity Trainer"
             path="/"
             />
           </ul>
