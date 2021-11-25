4337a68bde852a3f88513c13e9fbedf8f6e6a465
Moradi1412 <moradi.javid@gmail.com>
2021-11-24 17:48:20 -0800

    added the checkout

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -29,7 +29,7 @@ function Signup(props) {
   };
 
   return (
-    <div className="container my-1">
+    <div className="container my-1 sign-up">
       <Link to="/login">
         ← Go to Login
       </Link>
